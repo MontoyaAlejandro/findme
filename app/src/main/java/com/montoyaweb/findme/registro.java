527976f822e44d318ae7004c05553c85a42bdb1d
Montoya\Sistemas <sistemas.tota@gmail.com>
2021-11-05 12:08:03 -0500

    Merge branch 'Lina' of github.com:MontoyaAlejandro/findme
    # Conflicts:
    #	.idea/gradle.xml

--- app/src/main/java/com/montoyaweb/findme/registro.java
+++ app/src/main/java/com/montoyaweb/findme/registro.java
@@ -2,7 +2,9 @@ package com.montoyaweb.findme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registro extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
     }
+
+    public void ir_Login(View view){
+        Intent irLogin = new Intent (this, Login.class);
+        startActivity(irLogin);
+    }
+
 }
