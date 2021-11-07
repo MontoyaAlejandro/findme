232f32a89b7f3b4084c9cbb927f1848a87ac61a8
Nerio Zarrate <giyux@hotmail.com>
2021-11-06 22:46:37 -0500

    Algunas Correciones y navegabilidad entre las pantallas. Branch Nerio

--- app/src/main/java/com/montoyaweb/findme/Lugares.java
+++ app/src/main/java/com/montoyaweb/findme/Lugares.java
@@ -2,7 +2,9 @@ package com.montoyaweb.findme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Lugares extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class Lugares extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lugares);
+    }
+
+
+    public void onClick(View view) {
+        Intent miIntent=null;
+        switch (view.getId()){
+            case R.id.btn_editar:
+
+            case R.id.btn_eliminar:
+                miIntent=new Intent(Lugares.this, lugares_edit_delete.class);
+
+                break;
+
+            case R.id.btn_agregar:
+                miIntent=new Intent(Lugares.this, lugares_add.class);
+
+        }
+        startActivity(miIntent);
+
+
     }
 }
--- app/src/main/java/com/montoyaweb/findme/lugares_add.java
+++ app/src/main/java/com/montoyaweb/findme/lugares_add.java
@@ -2,7 +2,9 @@ package com.montoyaweb.findme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class lugares_add extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class lugares_add extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lugares_add);
+    }
+
+    public void onClick(View view) {
+        Intent miIntent=new Intent(lugares_add.this, Lugares.class);
+        startActivity(miIntent);
     }
 }
