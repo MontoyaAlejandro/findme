36698e4aff65e23d3e9900ad260c7739411cfb1c
Nerio Zarrate <giyux@hotmail.com>
2021-11-07 16:59:44 -0500

    Activiti contacto.Branch nerio

--- app/src/main/java/com/montoyaweb/findme/contacto_avatar.java
+++ app/src/main/java/com/montoyaweb/findme/contacto_avatar.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class contacto extends AppCompatActivity {
+public class contacto_avatar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contacto);
+        setContentView(R.layout.activity_contacto_Avatar);
     }
 }
