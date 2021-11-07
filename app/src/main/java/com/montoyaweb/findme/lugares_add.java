579fc8a2be4f5d098d49d60bbf3a338f3c9e328a
Nerio Zarrate <giyux@hotmail.com>
2021-11-06 21:52:19 -0500

    Correcion errores en vista.Branch Nerio

---
+++ app/src/main/java/com/montoyaweb/findme/lugares_add.java
@@ -0,0 +1,14 @@
+package com.montoyaweb.findme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lugares_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lugares_add);
+    }
+}
---
+++ app/src/main/java/com/montoyaweb/findme/lugares_edit_delete.java
@@ -0,0 +1,14 @@
+package com.montoyaweb.findme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lugares_edit_delete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lugares_edit_delete);
+    }
+}
